354c205f294c245901310e84f0029047bb1621db
pragati-2024 <bpragati21@gmail.com>
2025-06-26 18:17:44 +0530

    My javascript learning

---
+++ angaarlearning.js
@@ -0,0 +1,117 @@
+// console.log("PRAGATI");
+// input
+
+// let a = prompt("hi sharad write something");
+// console.log(a);
+
+// alert(a);
+
+// let a=1;
+// let b ='hi';
+// let c = true;
+// console.log(a,b,c);
+
+
+
+let look=123;
+
+let n = 123;
+
+// null,"",undefined,0=not a number,NAN-not a number
+// if(look == "good"){
+//     console.log("pretty");
+
+// }
+// else{
+//     console.log("ugly");
+// }
+
+// strict equality story of triple equality
+// if(look === n){
+//     console.log("pretty");
+// }
+
+// let a = (20 > 10)?"yes":"no";
+// console.log(a);
+
+// wednesday
+//  let first="sharad";
+//  let last = "chauhuan";
+//  let full = first +" "+ last;
+//  console.log(full);
+
+//  multiple concat krne
+// 3rdd way
+// let middle = "singh";
+// let c = `Sharad ${middle} agrawal`;
+// console.log(c);
+
+// functions-
+// 1. Default function
+
+
+prompt("please enter your name");
+let a=24;
+console.log(a); 
+// let first = "pragati";
+// let second = "Bansal";
+// let fullname = first +" "+ second;
+// console.log(fullname);
+
+let middle = "bansal";
+let c = `pragati ${middle} bansal`;
+console.log(c);
+
+let b = (20>10)?"yes":"no";
+console.log(b);
+
+let string1 = "sharad"
+let string2 = 'sharad'
+let string3 = `sharad`
+// console.log(string1,string2,string3);
+
+
+// concatination
+// let ans = `my name is ${string1} ${string2} ${string3} thanku}`
+// console.log(ans);
+
+//normal functions
+// function shello(){
+//     console.log("hi");
+// }
+// shello();
+
+// parametrised function
+// function jprgti(say){
+//     console.log(say);
+// }
+// say("hiiii");
+
+// function jovac(num,say){
+//     console.log(num,say);
+//     return num+say;
+// }
+// let res = jovac("Hi Mohit",100);
+
+// console.log(res);
+
+
+// function sayHi(say=20,num){
+
+//     console.log(say);
+//     console.log(num);
+
+//     console.log(say+num);
+
+// }
+// sayHi();
+
+// Power of Function
+let angaar = function (say){
+    console.log(say);
+}
+angaar("Good morning");
+
+
+
+
